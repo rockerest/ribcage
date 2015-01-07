18d095759ed91d2921b53939fd28feb21892d92b
rockerest <websites@thomasrandolph.info>
2015-01-07 00:34:24 -0500

    Non-region rendering areas
    Allow views to be rendered into a region without sub-regions
    Add ".intercostal" method to arbitrarily set the element of a view to a
    part of the region

--- src/region.js
+++ src/region.js
@@ -3,30 +3,56 @@ define(
     function( _ ){
         var Region = function( parent, selector ){
             this.output = parent.find( selector );
-
+            
+            this._isRendered = false;
             this._renderable = undefined;
             this._viewData = {};
         };
-
+        
         Region.prototype.render = function(){
             this._renderable.prototype.el = this.output;
-
+            
             var regionRenderable = new this._renderable( this._viewData ),
                 layout;
-
+            
+            this._isRendered = true;
+            
             if( _( regionRenderable ).has( "_regions" ) ){
                 regionRenderable.setElement( this.output );
                 layout = regionRenderable.render();
-
+                
                 this.regions = layout.regions;
             }
+            
         };
-
+        
         Region.prototype.show = function( renderable, optionalViewData ){
             this._renderable = renderable;
             this._viewData = optionalViewData;
-
+            
             this.render();
+        };
+        
+        Region.prototype.intercostal = function( view, selector ){
+            var el = this.output.find( selector ),
+                element;
+            
+            if( !this._isRendered ){
+                throw new Error( "Intercostal views will not work until this region has been rendered." );
+            }
+            
+            if( el.length > 0 ){
+                element = el[0];
+            }
+            
+            if( view.$el ){
+                view.setElement( element );
+            }
+            else{
+                view.prototype.el = element;
+            }
+            
+            return view;
         };
 
         return Region;
