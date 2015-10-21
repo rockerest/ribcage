cd888d9200d89821700161148e64edbed9de8374
rockerest <websites@thomasrandolph.info>
2015-10-20 22:59:24 -0600

    Better build process; NPM only

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,91 +1,29 @@
 module.exports = function( grunt ){
-    grunt.initConfig({
-        "notify": {
-            'requirejs': {
-                "options": {
-                    "title": 'Build Succeeded',
-                    "message": 'RequireJS has finished compiling',
-                }
-            }
-        },
-        "bower": {
-            "install":{
-                "options":{
-                    "layout": "byComponent",
-                    "targetDir": "./vendor",
-                    "cleanBowerDir": true,
-                    "verbose": true
-                }
-            }
-        },
-        "jshint":{
-            "main": {
-                "src": ["src/**/*.js"],
-            }
-        },
-        "requirejs":{
-            "compile":{
-                "options":{
-                    "baseUrl": "src/",
-                    "paths": {
-                        // LIBRARIES
-                        "backbone":             "../vendor/backbone/backbone",
-                        "underscore":           "../vendor/underscore/underscore",
-                        "jquery":               "empty:"
-                    },
-                    "shim": {
-                        "backbone": {
-                            "exports": "Backbone"
-                        },
-                        "underscore": {
-                            "exports": "_"
-                        }
-                    },
-                    "optimize": "uglify2",
-                    "preserveLicenseComments": false,
-                    "name": "ribcage",
-                    "out": "build/ribcage.js"
-                }
-            }
-        },
-        "watch": {
-            "js": {
-                "files": ['src/**/*.js'],
-                "tasks": ['code']
-            }
+    var config = require( "load-grunt-configs" )( grunt, {
+        "config": {
+            "src": "conf/grunt/*.*"
         }
-    });
+    } );
 
-    // contrib tasks
-    grunt.loadNpmTasks( 'grunt-contrib-watch' );
-    grunt.loadNpmTasks( 'grunt-contrib-requirejs' );
-    grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-
-    // Non-contrib tasks
-    grunt.loadNpmTasks( 'grunt-bower-task' );
-    grunt.loadNpmTasks( 'grunt-notify' );
+    require( "load-grunt-tasks" )( grunt );
+    grunt.initConfig( config );
 
     // Custom tasks
-    grunt.registerTask( 'clean', "Wipe the build directory", function(){
+    grunt.registerTask( "clean", "Wipe the build directory", function(){
         grunt.file.delete( "./build" );
+        grunt.file.delete( "./vendor" );
+    } );
+
+    grunt.registerTask( "prepare", "Prepare directory structure for anything necessary", function(){
+        grunt.task.run( [ "clean" ] );
         grunt.file.mkdir( "./build" );
-    });
-
-    grunt.registerTask( 'prepare', "Prepare directory structure for anything necessary", function(){
-        grunt.task.run( ['clean'] );
         grunt.file.mkdir( "./vendor" );
-    });
+    } );
 
-    grunt.registerTask( 'code', 'Compile the code', function(){
-        grunt.task.run(['jshint:main', 'requirejs:compile', 'notify:requirejs']);
-    });
+    grunt.registerTask( "code", "Compile the code", function(){
+        grunt.task.run( [ "requirejs", "uglify" ] );
+    } );
 
-    grunt.registerTask( 'build', 'Do a system build', function(){
-        grunt.task.run(['code']);
-    });
-
-
-
-    grunt.registerTask( 'setup', ['prepare', 'bower:install'] );
-    grunt.registerTask( 'default', ['build', 'watch'] );
+    grunt.registerTask( "setup", [ "prepare" ] );
+    grunt.registerTask( "default", [ "code" ] );
 };
